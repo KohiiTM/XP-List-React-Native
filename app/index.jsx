06b11784333c4983d953a3ae02ee0ec8952611a3
KohiiTM <jamesandallen.dev@gmail.com>
2025-07-06 18:49:51 -0700

    Update: expo dependencies

--- app/index.jsx
+++ app/index.jsx
@@ -1,1 +1,12 @@
-index.js
+import { StyleSheet, Text, View } from 'react-native'
+const Home = () => {
+	return (
+		<View>
+			<Text>Home</Text>
+		</View>
+	)
+}
+export default Home
+
+const styles = StyleSheet.create({})
+
